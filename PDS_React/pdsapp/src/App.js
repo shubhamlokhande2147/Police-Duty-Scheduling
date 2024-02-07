c1fd2367cc4b5b4ea8fcf52228c8c8bbfd48a0a3
ShubhamLokhande2147 <shubhamlokhande2147@gmail.com>
2024-02-07 12:46:48 +0530

    Login Form edit

--- PDS_React/pdsapp/src/App.js
+++ PDS_React/pdsapp/src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
+import HomeComponent from './pages/HomeComponent';
+import Header from './Components/Header';
+
 import './App.css';
+import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Header></Header>
+     {/* <Header></Header>
+      <MainNavBar></MainNavBar>
+      <Routes>
+        <Route path="/home" element={<HomeComponent></HomeComponent>}></Route>
+        <Route path="/table" element={<ProductTable></ProductTable>}></Route>
+        <Route path="/form" element={<ProductForm></ProductForm>}></Route>
+        <Route path="/list" element={<ProductList></ProductList>}></Route>
+        <Route path="/edit/:id" element={<ProductEdit></ProductEdit>}></Route>
+        <Route path="/view/:id" element={<ProductDetails></ProductDetails>}></Route>
+      </Routes>
+      <Footer></Footer> */}
+  <HomeComponent></HomeComponent>
+  <LoginPage></LoginPage>
+  
     </div>
   );
 }
---
+++ PDS_React/pdsapp/src/Components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Footer.css'
+export default function Header() {
+  return (
+    <div>
+        <h1 className="header">Police-Duty-Scheduling</h1>
+    </div>
+  )
+}
