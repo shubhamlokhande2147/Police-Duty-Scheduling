e46538003827fa518cf30ada6791df0b3220a7a6
rohitr99 <rohitrshende5@gmail.com>
2024-02-21 02:52:47 +0530

    slogans added

--- PDS_React/pdsapp/src/Components/HomeComponent.js
+++ PDS_React/pdsapp/src/Components/HomeComponent.js
@@ -32,18 +32,24 @@ export default function HomeComponent() {
           <section className="cards col">
             <div className="card">
               <img src="C:\Users\91702\Desktop\pdssss\Police-Duty-Scheduling\PDS_React\pdsapp\public\images\card1.jpeg" alt="Card 1" />
-              <h3>Card 1 Title</h3>
-              <p>Card 1 Description</p>
+              <h3>"सद्रक्षणाय खलनिग्रहणाय"</h3>
+              <p>This slogan refered from Bhagvat geeta and its meaning is "Maharashtra police protect guiltless and distroy 
+harmful people by controlling them."
+</p>
             </div>
             <div className="card col">
               <img src="C:\Users\91702\Desktop\pdssss\Police-Duty-Scheduling\PDS_React\pdsapp\public\images\card2.jpeg" alt="Card 2" />
-              <h3>Card 2 Title</h3>
-              <p>Card 2 Description</p>
+              <h3>Determined</h3>
+              <p>Our real heros is the real identity of our society . Maharashtra police is not just a name its a dedicated, tallented, bravery group
+of police officers who not only protect us but maintain our daily life on track...our project is a small contribution to help them for scheduling their duty
+in a easy way so that they will focus on major cases. Maharashtra police is a jersy of pride and emotions for all people for their work and dedication salute to 
+police officers!</p>
             </div>
             <div className="card col">
-              <img src="C:\Users\91702\Desktop\pdssss\Police-Duty-Scheduling\PDS_React\pdsapp\public\images\card3.jpeg" alt="Card 3" />
-              <h3>Card 3 Title</h3>
-              <p>Card 3 Description</p>
+              <img src="C:\Users\91702\Desktop\pdssss\Police-Duty-Scheduling\PDS_React\pdsapp\public\images\card3.jpg" alt="Card 3" />
+              <h3>Patriotic</h3>
+              <p>Smiling face of police officers is the satisfied emotions to our society cause they smile when their assigned duty is accomplish. they are the key root of our 
+   society...proud to our heros.</p>
             </div>
           </section>
           </div>
