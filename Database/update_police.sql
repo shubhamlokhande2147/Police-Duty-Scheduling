0901d2445779a6be5ea6dfa7a9ba677911fa8ba9
ShubhamLokhande2147 <shubhamlokhande2147@gmail.com>
2024-02-24 23:18:11 +0530

    add update_police

---
+++ Database/update_police.sql
@@ -0,0 +1,18 @@
+-- Drop the trigger if it exists
+DROP TRIGGER IF EXISTS update_police;
+
+-- Change the delimiter to avoid conflicts with semicolons inside the trigger definition
+DELIMITER $$
+
+-- Create the trigger
+CREATE TRIGGER update_police AFTER UPDATE ON police
+FOR EACH ROW
+BEGIN
+    -- Update the `role_id` in the `login` table where the `username` and `password` match
+    UPDATE login 
+    SET role_id = 3 
+    WHERE username = OLD.username AND password = OLD.password;
+END$$
+
+-- Reset the delimiter back to semicolon
+DELIMITER ;
