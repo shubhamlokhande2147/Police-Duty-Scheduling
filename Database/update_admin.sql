9b672d641b850606ba2666f3738ee4885394f667
ShubhamLokhande2147 <shubhamlokhande2147@gmail.com>
2024-02-26 19:16:15 +0530

    add triggers update_admin

--- Database/update_admin.sql
+++ Database/update_admin.sql
@@ -1,16 +1,16 @@
 -- Drop the trigger if it exists
-DROP TRIGGER IF EXISTS update_am;
+DROP TRIGGER IF EXISTS update_admin;
 
 -- Change the delimiter to avoid conflicts with semicolons inside the trigger definition
 DELIMITER $$
 
 -- Create the trigger
-CREATE TRIGGER update_am AFTER UPDATE ON attendance_master
+CREATE TRIGGER update_admin AFTER UPDATE ON admin
 FOR EACH ROW
 BEGIN
     -- Update the `role_id` in the `login` table where the `username` and `password` match
     UPDATE login 
-    SET role_id = 2
+    SET role_id = 1 
     WHERE username = OLD.username AND password = OLD.password;
 END$$
 
